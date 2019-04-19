df5d9238bc2cfbbb7f47681def41f6954171fd62
Patrick Duvall <duvallp208@gmail.com>
2019-04-19 14:58:19 -0600

    Add link and page for individual articl

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "user sees one articles" do
   describe "they link from the articles index" do
     it "displays information for one article" do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article = Article.create!(title: "Title 1", body: "Body 1")
       visit '/articles'
       click_link article.title
       expect(page).to have_content(article.title)
